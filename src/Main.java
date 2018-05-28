f3f5da7c6008dd9df7f9403e7024a7e72242f9d3
Hxin <liuxin@jiuxian.com>
2018-05-28 22:37:20 +0800

    unique Morse Representations

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
@@ -10,11 +14,13 @@ public class Main {
         nums[3] = 15;
 
         int[] ints = twoSum(nums, 9);
-        for (int i:ints){
+        for (int i : ints) {
             System.out.println(ints[i]);
         }
         /*---------------------------------------------*/
-
+        String[] words ={"gin", "zen", "gig", "msg"};
+        int i = uniqueMorseRepresentations(words);
+        System.out.println(i);
 
     }
 
@@ -26,6 +32,7 @@ public class Main {
      * Given nums = [2, 7, 11, 15], target = 9,
      * Because nums[0] + nums[1] = 2 + 7 = 9,
      * return [0, 1]
+     *
      * @since 2018-5-28
      */
     public static int[] twoSum(int[] nums, int target) {
@@ -33,7 +40,7 @@ public class Main {
             for (int j = i + 1; j < nums.length; j++) {
                 int sum = nums[i] + nums[j];
                 if (target == sum) {
-                    return new int[]{i,j};
+                    return new int[]{i, j};
                 }
             }
         }
@@ -43,32 +50,64 @@ public class Main {
     /**
      * International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes,
      * as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
-     For convenience, the full table for the 26 letters of the English alphabet is given below:
-
-     [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.",
-     "...","-","..-","...-",".--","-..-","-.--","--.."]
-
-     Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter.
-     For example, "cab" can be written as "-.-.-....-", (which is the concatenation "-.-." + "-..." + ".-").
-     We'll call such a concatenation, the transformation of a word.
-     Return the number of different transformations among all words we have.
-
-     Example:
-     Input: words = ["gin", "zen", "gig", "msg"]
-     Output: 2
-     Explanation:
-     The transformation of each word is:
-     "gin" -> "--...-."
-     "zen" -> "--...-."
-     "gig" -> "--...--."
-     "msg" -> "--...--."
-
-     There are 2 different transformations, "--...-." and "--...--.".
-
-     Note:
-
-     The length of words will be at most 100.
-     Each words[i] will have length in range [1, 12].
-     words[i] will only consist of lowercase letters.
+     * For convenience, the full table for the 26 letters of the English alphabet is given below:
+     * <p>
+     * [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.",
+     * "...","-","..-","...-",".--","-..-","-.--","--.."]
+     * <p>
+     * Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter.
+     * For example, "cab" can be written as "-.-.-....-", (which is the concatenation "-.-." + "-..." + ".-").
+     * We'll call such a concatenation, the transformation of a word.
+     * Return the number of different transformations among all words we have.
+     * <p>
+     * Example:
+     * Input: words = ["gin", "zen", "gig", "msg"]
+     * Output: 2
+     * Explanation:
+     * The transformation of each word is:
+     * "gin" -> "--...-."
+     * "zen" -> "--...-."
+     * "gig" -> "--...--."
+     * "msg" -> "--...--."
+     * <p>
+     * There are 2 different transformations, "--...-." and "--...--.".
+     * <p>
+     * Note:
+     * <p>
+     * The length of words will be at most 100.
+     * Each words[i] will have length in range [1, 12].
+     * words[i] will only consist of lowercase letters.
+     * @since 2018-5-28
      */
+    public static int uniqueMorseRepresentations(String[] words) {
+        String[] str = new String[]{".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+                "....", "..", ".---", "-.-", ".-..", "--", "-.",
+                "---", ".--.", "--.-", ".-.", "...", "-",
+                "..-", "...-", ".--", "-..-", "-.--", "--.."};
+        String[] word = new String[]{"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+        Map<String,String> map = new HashMap<>();
+        for (int i =0;i<str.length;i++){
+            map.put(str[i],word[i]);
+        }
+        List<StringBuilder> list = new ArrayList<>();
+        for (String i : words) {
+            StringBuilder sb = new StringBuilder();
+            char[] chars = i.toCharArray();
+            for (char c : chars) {
+                for (String s:str){
+                    //比较两个char 是否相等
+                    if (map.get(s).equals(c)){
+                        sb.append(s);
+                    }
+                }
+            }
+            list.add(sb);
+        }
+        int number = 0;
+        for (StringBuilder sb:list){
+            list.contains(sb);
+            number++;
+        }
+        return number-list.size();
+    }
 }
