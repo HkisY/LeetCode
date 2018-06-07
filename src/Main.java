3ada297ebb333812ffc2a421e9a850eca0a82258
Hxin <liuxin@jiuxian.com>
2018-06-07 18:35:40 +0800

    merge binary trees

--- src/Main.java
+++ src/Main.java
@@ -40,14 +40,27 @@ public class Main {
 //        int i = hammingDistance(1, 4);
 //        System.out.println(i);
         /*---------------------------------------------*/
-        String s = "UDUDUDUDUDUDLLLLLLRRRRRR";
-        boolean b = judgeCircle(s);
-        System.out.println(b);
-        String move = "LL";
-        boolean flag = judgeCircle(move);
-        System.out.println(flag);
+//        String s = "UDUDUDUDUDUDLLLLLLRRRRRR";
+//        boolean b = judgeCircle(s);
+//        System.out.println(b);
+//        String move = "LL";
+//        boolean flag = judgeCircle(move);
+//        System.out.println(flag);
+        /*---------------------------------------------*/
+        TreeNode a = new TreeNode(5);
+        TreeNode b = new TreeNode(3, a, null);
+        TreeNode f = new TreeNode(2);
+        //主节点
+        TreeNode c = new TreeNode(1, b, f);
 
-
+        TreeNode l = new TreeNode(4);
+        TreeNode m = new TreeNode(1, null, l);
+        TreeNode n = new TreeNode(7);
+        TreeNode o = new TreeNode(3, null, n);
+        //主节点
+        TreeNode p = new TreeNode(2, m, o);
+        TreeNode resultTree = mergeTrees(c, p);
+        System.out.println(resultTree);
 
     }
 
@@ -320,5 +333,75 @@ public class Main {
             return true;
         }
         return false;
+    }
+
+    /**
+     * Given two binary trees and imagine that when you put one of them to cover the other,
+     * some nodes of the two trees are overlapped while the others are not.
+     * <p>
+     * You need to merge them into a new binary tree. The merge rule is that if two nodes overlap,
+     * then sum node values up as the new value of the merged node. Otherwise,
+     * the NOT null node will be used as the node of new tree.
+     * <p>
+     * Example 1:
+     * Input:
+     * Tree 1                     Tree 2
+     *     1                         2
+     *    / \                       / \
+     *   3   2                     1   3
+     *  /                           \   \
+     * 5                             4   7
+     * Output:
+     * Merged tree:
+     *     3
+     *    / \
+     *   4   5
+     *  / \   \
+     * 5   4   7
+     * Note: The merging process must start from the root nodes of both trees.
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public String toString() {
+            return "TreeNode{" +
+                    "val=" + val +
+                    ", left=" + left +
+                    ", right=" + right +
+                    '}';
+        }
+    }
+
+    public static TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null) {
+            return t2;
+        }
+        if (t2 == null) {
+            return t1;
+        }
+        t1.val += t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
     }
 }
