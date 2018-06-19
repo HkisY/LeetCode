c8a86856f1e3267a9ecf636571bbf2a7612db21a
Hxin <liuxin@jiuxian.com>
2018-06-19 10:50:47 +0800

    Subdomain Visit Count

--- src/Main.java
+++ src/Main.java
@@ -71,10 +71,17 @@ public class Main {
 //        quickSort(num);
 //        int resualt = arrayPairSum(num);
         /*---------------------------------------------*/
-        int[] widths = {4,7,6,4,10,5,3,10,10,7,4,4,6,7,10,10,8,7,2,6,3,3,4,7,2,4};
-        String S = "kjvswlxlss";
-        int[] ints = numberOfLines(widths, S);
-        System.out.println(Arrays.toString(ints));
+//        int[] widths = {4,7,6,4,10,5,3,10,10,7,4,4,6,7,10,10,8,7,2,6,3,3,4,7,2,4};
+//        String S = "kjvswlxlss";
+//        int[] ints = numberOfLines(widths, S);
+//        System.out.println(Arrays.toString(ints));
+        /*---------------------------------------------*/
+//        String[] s = {"900 jiuxian.guangdong.com","12 hxin.com"};
+//        List<String> list = subdomainVisits(s);
+//        for (String s1:list){
+//            System.out.println(s1);
+//        }
+        /*---------------------------------------------*/
 
     }
 
@@ -711,5 +718,88 @@ public class Main {
 
         }
         return new int[]{line, units};
+    }
+
+    /**
+     * A website domain like "discuss.leetcode.com" consists of various subdomains.
+     * At the top level, we have "com", at the next level, we have "leetcode.com",
+     * and at the lowest level, "discuss.leetcode.com". When we visit a domain like "discuss.leetcode.com",
+     * we will also visit the parent domains "leetcode.com" and "com" implicitly.
+
+     Now, call a "count-paired domain" to be a count (representing the number of visits this domain received),
+     followed by a space, followed by the address.
+     An example of a count-paired domain might be "9001 discuss.leetcode.com".
+
+     We are given a list cpdomains of count-paired domains.
+     We would like a list of count-paired domains,
+     (in the same format as the input, and in any order),
+     that explicitly counts the number of visits to each subdomain.
+
+     Example 1:
+     Input:
+     ["9001 discuss.leetcode.com"]
+     Output:
+     ["9001 discuss.leetcode.com", "9001 leetcode.com", "9001 com"]
+     Explanation:
+     We only have one website domain: "discuss.leetcode.com".
+     As discussed above, the subdomain "leetcode.com" and "com" will also be visited.
+     So they will all be visited 9001 times.
+
+     Example 2:
+     Input:
+     ["900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"]
+     Output:
+     ["901 mail.com","50 yahoo.com","900 google.mail.com","5 wiki.org","5 org","1 intel.mail.com","951 com"]
+     Explanation:
+     We will visit "google.mail.com" 900 times, "yahoo.com" 50 times,
+     "intel.mail.com" once and "wiki.org" 5 times. For the subdomains,
+     we will visit "mail.com" 900 + 1 = 901 times, "com" 900 + 50 + 1 = 951 times, and "org" 5 times.
+
+     Notes:
+
+     The length of cpdomains will not exceed 100.
+     The length of each domain name will not exceed 100.
+     Each address will have either 1 or 2 "." characters.
+     The input count in any count-paired domain will not exceed 10000.
+     The answer output can be returned in any order.
+     */
+    public static List<String> subdomainVisits(String[] cpdomains) {
+        Map<String, Integer> middle = new HashMap();
+        for (String domain : cpdomains) {
+            String[] cpinfo = domain.split("\\s+");
+            String[] word = cpinfo[1].split("\\.");
+            int count = Integer.valueOf(cpinfo[0]);
+            String str = "";
+            for (int i = word.length - 1; i >= 0; --i) {
+                str = word[i] + (i < word.length - 1 ? "." : "") + str;
+                //jdk 1.8 中新增方法  不需要像以前一样 增加set 判断是否存在
+                middle.put(str, middle.getOrDefault(str, 0) + count);
+            }
+        }
+
+        List<String> list = new ArrayList();
+        for (String s : middle.keySet())
+            list.add(middle.get(s) + " " + s);
+        return list;
+    }
+
+    /**
+     * Given a string S and a character C,
+     * return an array of integers representing the shortest distance from the character C in the string.
+
+     Example 1:
+
+     Input: S = "loveleetcode", C = 'e'
+     Output: [3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0]
+
+
+     Note:
+
+     S string length is in [1, 10000].
+     C is a single character, and guaranteed to be in string S.
+     All letters in S and C are lowercase.
+     */
+    public static int[] shortestToChar(String S, char C) {
+        return null;
     }
 }
